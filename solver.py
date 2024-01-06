0fddfb410570ce77e74bd3e076b687db77a2d958
Kunal wagh <kunal.wagh@enine.school>
2024-01-06 17:27:39 +0530

    new commit

--- solver.py
+++ solver.py
@@ -1,5 +1,6 @@
 """prime numbers upto using"""
 
+
 def fib(b):
     """fib"""
     m = 1
@@ -13,7 +14,11 @@ def fib(b):
 
 def nested_prime(n):
     """nested prime"""
-    li = [i for i in range(2, n + 1) if all(i % j != 0 for j in range(2, int(i**0.5) + 1))]
+    li = [
+        i
+        for i in range(2, n + 1)
+        if all(i % j != 0 for j in range(2, int(i**0.5) + 1))
+    ]
     return li
 
 
@@ -25,8 +30,13 @@ def old_school_reverse(n):
 
 def dict_a_noodle(a):
     """dict_a_noodle"""
-    #return {value: key if isinstance(key, str) else value for key, value in a.items()}
-    return { (value if isinstance(key,str) else key ):(key if isinstance(value,int) else value) for key, value in a.items()}
+    # return {value: key if isinstance(key, str) else value for key, value in a.items()}
+    return {
+        (value if isinstance(key, str) else key): (
+            key if isinstance(value, int) else value
+        )
+        for key, value in a.items()
+    }
 
 
 def rec(val):
@@ -44,10 +54,10 @@ def dict_of_lists(data):
     """dict_of_lists"""
     array = rec(data)
     main = {}
-    n_main = { i:array.count(i) for i in array }
+    n_main = {i: array.count(i) for i in array}
     # for i in array:
     #     main[i] = main.get(i, 0) + 1
-    print("new main" ,n_main)
+    print("new main", n_main)
     return main
 
 
@@ -58,55 +68,116 @@ def list_of_lists(data):
     end = sorted([i for i in array if isinstance(i, str)], key=len)
     return start + end
 
+
 def fib_squares(a, b):
     """fib_squares"""
     li = [i**2 if i in fib(b) else i for i in range(a, b + 1)]
     return li
 
+
 def set_complement(*args, verbose=True):
     """set_comlement"""
-    #ans = [ [j for j in args[i] if j not in args[i+1] ] for i in range(len(args)-1)]
-    #ans = [[ j for j in range(len(str(i))) if args[i][j] not in args[((i*-1)-1)]  ] for i in range(len(args))]
+    # ans = [ [j for j in args[i] if j not in args[i+1] ] for i in range(len(args)-1)]
+    """ans = [[j for j in range(len(str(i))) if args[i][j] not in args[((i*-1)-1)]] for i in range(len(args))]"""
     a = [set(i) for i in args]
-    ans = [  list(a[i] - a[j]) for i in range(len(str(a))) for j in range(i+1,len(a))  ]
+    ans = [list(a[i] - a[j]) for i in range(len(str(a))) for j in range(i + 1, len(a))]
 
     if verbose:
-        for i in args :
-            ans.append(i)     
+        for i in args:
+            ans.append(i)
         return ans
-    else :   
+    else:
         return ans
 
-        
+
 def set_intersection(*args, verbose=True):
     """set_int"""
     a = [set(i) for i in args]
-    ans = [  list(a[i] & a[j]) for i in range(len(str(a))) for j in range(i+1,len(a))  ]
+    ans = [list(a[i] & a[j]) for i in range(len(str(a))) for j in range(i + 1, len(a))]
     if verbose:
-        for i in args :
-            ans.append(i)     
+        for i in args:
+            ans.append(i)
         return ans
-    else :   
+    else:
         return ans
-    
-def checK_equal(dict1 ,dict2):
-    if isinstance(dict1,dict2) :
+
+
+def checK_equal(dict1, dict2):
+    """set_int"""
+    if isinstance(dict1, dict2):
         return -1
 
-        
-def dict_compare(*args):
-    for  i in range(len(args)) :
-        for j in range(i+1,len(args)) :
-            check = checK_equal(args[i] , args[j])
-            return -1
-            
-            #base code 
-            
 
+def dict_compare(*args):
+    """set_int"""
+    for i in range(len(args)):
+        for j in range(i + 1, len(args)):
+            check = checK_equal(args[i], args[j])
+            return -1
+
+            # base code
+
+
+def dict_from_lists(list1, list2):
+    """set_int"""
+    # data = { (i for i in list1 ) : ( str(j) for j in list2) for k in range(len(list1)+1)  }
+    if len(list1) < len(list2):
+        limit = list2
+    else:
+        limit = list1
+    """extra code , this condition can be put in dict comprihension if required"""
+
+    data = {list1[i]: list2[i] if list2[i] else " " for i in range(len(limit))}
+    return data
+
+
+def my_secret(message):
+    """set_int"""
+    if len(message) > 81:
+        message = message[:81]
+    mess = message.replace(" ", "")
+    secret_message = ""
+    secret_message += "\n".join(mess[i : i + 7] for i in range(0, len(mess), 7))
+    # secret_message += f'{mess[i:i+7] for i in range(0,len(mess),7)} '
+    # li = [mess[i:i+7]  for i in range(0,len(mess),7) ]
+    return secret_message
+
+
+def cal(data, ph):
+    """set_int"""
+    avl = [data[int(i)] for i in ph]
+    ans = []
+    for m in range(len(avl)):
+        for j in range(len(avl[m])):
+            f_sum = avl[m][j]
+            s_sum = " "
+            for i in range(m + 1, len(avl)):
+                s_sum += avl[i][j]
+        f_sum += s_sum
+        ans.append(f_sum)
+    return ans
+
+
+def phone_words(ph1, ph2):
+    """set_int"""
+    data = {
+        2: "ABC",
+        3: "DEF",
+        4: "GHI",
+        5: "JKL",
+        6: "MNO",
+        7: "PRS",
+        8: "TUV",
+        9: "WXY",
+    }
+
+    # xymat = [0,1,"ABC","DEF","GHI","jKL","MNO","PRS","TUV","WXY"]
+    output = {i: cal(data, i) if "1" not in i or "0" not in i  else [] for i in [ph1, ph2]}
+    return output
 
 
 if __name__ == "__main__":
-    b= {"one": 1, "two": 2, 3: "three"}
+    b = {"one": 1, "two": 2, 3: "three"}
     print(nested_prime(10))
     print(old_school_reverse("hiamkunal"))
     print(dict_a_noodle(b))
@@ -118,5 +189,14 @@ if __name__ == "__main__":
     ]
     print(dict_of_lists(data_new))
     print(list_of_lists(data_new))
-    print(set_complement([2,5,6],[6,9,1],[5,8,9]))
-    print(set_intersection([2,5,6],[6,9,1],[5,8,9]))
+    print(set_complement([2, 5, 6], [6, 9, 1], [5, 8, 9]))
+    print(set_intersection([2, 5, 6], [6, 9, 1], [5, 8, 9]))
+    list1 = [1, 2, 3]
+    list2 = ["a", "b", "c"]
+    print(dict_from_lists(list1, list2))
+    print(
+        my_secret(
+            "If man was meant to stay on the ground god would have given us roots"
+        )
+    )
+    print(phone_words("1234567", " 2345678"))
